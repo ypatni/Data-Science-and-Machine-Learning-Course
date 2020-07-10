1d94355f7d08f31abe52344e046831bce6210ad3
Yash Patni <ypatni3@gmail.com>
2020-07-09 21:28:19 -0400

    second half of exercises

--- Python/Pandas/SalariesExercise.py
+++ Python/Pandas/SalariesExercise.py
@@ -19,5 +19,32 @@ print(f"\n")
 print(sal[sal [ 'TotalPayBenefits'] == sal['TotalPayBenefits'].max()]['EmployeeName'])
 print(f"\n")
 
+print(sal[sal [ 'TotalPayBenefits'] == sal['TotalPayBenefits'].min()])
+print(f"\n")
+print(sal.groupby('Year').mean()['BasePay'])
+print(f"\n")
+print(sal['JobTitle'].nunique())
+print(f"\n")
+print(sal['JobTitle'].value_counts().head(5))
+print(f"\n")
+# How many Job Titles were represented by only one person in 2013? (e.g. Job Titles with only one occurence in 2013?)
+print(sum(sal[sal['Year'] == 2013]['JobTitle'].value_counts() ==1 )) 
+#How many people have the word Chief in their job title? (This is pretty tricky)
+print(f"\n")
+#print(sum(sal['JobTitle'].str.lower().str.contains('chief'))) - one solution 
+def chief_name(title):
+    if 'chief' in title.lower():
+
+        return True
+
+    else:
+
+        return False  
+
+print(sum(sal['JobTitle'].apply(lambda x: chief_name(x))))
+
+#Is there a correlation between length of the Job Title string and Salary?
+sal['title_len'] = sal['JobTitle'].apply(len)
+print(sal[['TotalPayBenefits', 'title_len']].corr())
 
 
