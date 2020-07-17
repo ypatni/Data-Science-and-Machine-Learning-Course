2931398e53ae0d7133e9a9002ae5d32b5571753d
Yash Patni <ypatni3@gmail.com>
2020-07-16 21:14:15 -0400

    finding intercepts and coefficients of the training variables

--- Intro To Machine Learning/Linear Regression/Intro.py
+++ Intro To Machine Learning/Linear Regression/Intro.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import chart_studio.plotly as py 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston #for real dataset 
 mpl.rcParams['patch.force_edgecolor'] = True
 sns.set()
 
@@ -31,7 +32,6 @@ y = df['Price']
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=101)
 # we basically use tuple unpacking to get your training and testing variables, test size is the percentage of data set allocated to the test size, 
 # random state ensures a specific set of random splits on data 
-
 lm = LinearRegression()
 lm.fit(x_train, y_train)
 print(f"\n")
@@ -39,5 +39,8 @@ print(lm.intercept_)
 print(f"\n")
 print(lm.coef_) #returning coefficients the coefficients relate to the columns in x_train
 cdf = pd.DataFrame(lm.coef_, x.columns, columns=['Coef']) #making a dataframe using the coefficients so that we can visualize and use them
-print(cdf)
+print(cdf) 
+# so if we hold all other features fixed, a one unit increase in average income is associated with an increase in $21.53 in price
+# the data we have is artificial and makes literally no sense
+ 
 
