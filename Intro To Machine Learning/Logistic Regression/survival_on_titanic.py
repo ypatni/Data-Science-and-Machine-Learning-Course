2115edbc1e09548d12ec9b8ce9e89974f3946cc3
Yash Patni <ypatni3@gmail.com>
2020-07-18 16:28:27 -0400

    cleaning data for ML algorithms

--- Intro To Machine Learning/Logistic Regression/survival_on_titanic.py
+++ Intro To Machine Learning/Logistic Regression/survival_on_titanic.py
@@ -9,9 +9,10 @@ sns.set()
 train = pd.read_csv('titanic_train.csv')
 print(train.head())
 print(train.isnull()) 
-#making a heatmap 
+#making a heatmap to figure out missing data 
 sns.heatmap(train.isnull(), yticklabels=False, cbar = False, cmap= 'viridis')
 plt.show() #missing data in age column, cabin column and embarked 
+
 sns.set_style('whitegrid')
 sns.countplot(x = 'Survived', hue='Sex',  data=train, palette = 'RdBu_r')
 plt.show()
@@ -24,3 +25,30 @@ sns.countplot(x = 'SibSp', data = train)
 plt.show()
 train['Fare'].plot.hist(bins=40, figsize= (10,4))
 plt.show()
+
+#cleaning data for ML algorithms 
+# using average age by passenger class in order to fill in missing age data (imputation)
+plt.figure(figsize=(10,7))
+sns.boxplot(x='Pclass', y = 'Age', data = train)
+plt.show() #shows that wealthier classes have older people 
+
+def impute_age(cols):
+    Age = cols[0]
+    Pclass = cols[1]
+    if pd.isnull(Age):
+        if Pclass == 1:
+            return 37
+        elif Pclass ==2:
+            return 29
+        else:
+            return 24
+    else: 
+        return Age
+
+train['Age'] = train[['Age', 'Pclass']].apply(impute_age, axis = 1)
+#checking again for missing age data 
+train.drop('Cabin', axis = 1, inplace = True)
+train.dropna(inplace=True)
+sns.heatmap(train.isnull(), yticklabels=False, cbar = False, cmap= 'viridis')
+plt.show() #now no missing age columns 
+
