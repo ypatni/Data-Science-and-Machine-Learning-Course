8e147e6be384b8e4b3c17ad7f92b8fdbd4c9caab
Yash Patni <ypatni3@gmail.com>
2020-07-22 21:53:05 -0400

    finding optimal k value using error rates

--- Intro To Machine Learning/K Nearest Neighbors/knn_project.py
+++ Intro To Machine Learning/K Nearest Neighbors/knn_project.py
@@ -20,4 +20,23 @@ print(df.head())
 scaler = StandardScaler()
 scaler.fit(df.drop('TARGET CLASS', axis = 1))
 
-scaled_feat = scaler.transform(df.drop('TARGET CLASS', axis = 1))
+scaled_features = scaler.transform(df.drop('TARGET CLASS', axis = 1))
+
+df_feat = pd.DataFrame(scaled_features, columns = df.columns[:-1]) 
+print(df_feat.head())
+x= df_feat 
+y= df['TARGET CLASS']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=101)
+error_rate = [] 
+for i in range(1,40):
+    knn = KNeighborsClassifier(n_neighbors=i)
+    knn.fit(x_train, y_train)
+    pred_i = knn.predict(x_test)
+    error_rate.append(np.mean(pred_i != y_test))
+
+plt.figure(figsize=(10,6))
+plt.plot(range(1,40), error_rate, color = 'green', linestyle = 'dashed', marker = 'o', markerfacecolor = 'blue', markersize = 10)
+plt.title('Error Rate vs K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
+plt.show()
