82f39f2033b36a37d66fd45eac6a03e1219e5c3f
Yash Patni <ypatni3@gmail.com>
2020-07-27 19:35:46 -0400

    improved accuracy using gridsearch to tune params

--- Intro To Machine Learning/Support Vector Machines/svm_iris_flower_dataset.py
+++ Intro To Machine Learning/Support Vector Machines/svm_iris_flower_dataset.py
@@ -32,3 +32,12 @@ print(classification_report(y_test, predictions))
 print('\n')
 print(confusion_matrix(y_test, predictions)) 
 
+param_grid = {'C': [0.1, 1,10,100,1000], 'gamma': [1,0.1,0.01,0.001,0.0001]}
+grid = GridSearchCV(SVC(), param_grid, verbose = 2)
+grid.fit(x_train, y_train)
+print(grid.best_params_)
+print(grid.best_estimator_)
+grid_predictions = grid.predict(x_test)
+print(classification_report(y_test, grid_predictions)) 
+print('\n')
+print(confusion_matrix(y_test, grid_predictions)) 
