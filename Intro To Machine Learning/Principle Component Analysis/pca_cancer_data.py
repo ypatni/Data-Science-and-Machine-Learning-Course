68599cb51d39557bd1a8470f21656c96082ee182
Yash Patni <ypatni3@gmail.com>
2020-07-30 14:41:57 -0400

    creating pca object and finding principle components

--- Intro To Machine Learning/Principle Component Analysis/pca_cancer_data.py
+++ Intro To Machine Learning/Principle Component Analysis/pca_cancer_data.py
@@ -6,7 +6,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_breast_cancer
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.decomposition import PCA 
 mpl.rcParams['patch.force_edgecolor'] = True
 sns.set()
 sns.set_style('whitegrid')
@@ -20,3 +20,17 @@ print(df.head())
 scaler = StandardScaler()
 scaler.fit(df)
 scaled_data = scaler.transform(df)
+
+#PCA 
+pca = PCA(n_components = 2)
+pca.fit(scaled_data)
+for i in range(0, 2):
+    for j in range(0, pca.components_[i].size):
+        pca.components_[i][j] = -1 * pca.components_[i][j]
+x_pca = pca.transform(scaled_data)
+plt.figure(figsize = (8,6))
+plt.scatter(x_pca[:,0],x_pca[:,1], c = cancer['target'], cmap= 'rainbow' )
+
+plt.xlabel('First Principle Component')
+plt.ylabel('Second Principle Component')
+plt.show()
