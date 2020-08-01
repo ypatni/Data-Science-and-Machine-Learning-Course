01be75dd97fc0ffd5132826b982fdad0f025404b
Yash Patni <ypatni3@gmail.com>
2020-08-01 11:32:29 -0400

    text preprocessing function

--- Intro To Machine Learning/Natural Language Processing/spam_filter.py
+++ Intro To Machine Learning/Natural Language Processing/spam_filter.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
+import string
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
 mpl.rcParams['patch.force_edgecolor'] = True
 sns.set()
 sns.set_style('whitegrid')
@@ -26,6 +29,29 @@ messages['length'] = messages['message'].apply(len)
 print(messages.head())
 messages['length'].plot.hist(bins=90)
 plt.show()#theres a really really long message that I want to find 
-#print(messages[messages['length'] ==910]['message'].iloc[0]) very creepy love letter don't look at it 
+#print(messages[messages['length'] ==910]['message'].iloc[0])#very creepy love letter don't look at it 
 messages.hist(column = 'length', by = 'label', bins = 60, figsize=(12,4))
 plt.show() #ham texts seem to be centered around 50 words while spam usually has around 150 words
+
+#TEXT PREPROCESSING
+#we need numercial feature vectors to use classification tasks
+
+#function that splits message into individual words and returns a list and remove common words like the, a, if
+
+#mess = "Sample Message. It has: punctuations!"
+#nopunct = [c for c in mess if c not in string.punctuation]
+#print(nopunct)
+#print(stopwords.words('English'))  - produces all the common words that probably don't affect the spam or ham detection
+#nopunct = ''.join(nopunct) #joining elements in the list together
+#print(nopunct) #now it has no punctuations 
+#clean_mess = [word for word in nopunct.split() if word.lower() not in stopwords.words('English')] #now we remove the stopwords 
+#print(clean_mess)
+
+def text_process(mess):
+    #remove punctuation
+    # remove stopwords
+    # return list of clean words
+    nopunc = [char for char in mess if char not in string.punctuation]
+    nopunc = ''.join(nopunc)
+    return [word for word in nopunc.split() if word.lower() not in stopwords.words('English')] 
+
