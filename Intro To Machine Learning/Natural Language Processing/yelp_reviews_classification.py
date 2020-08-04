d037ddf7d45ed71404d76aba6af0a7f0b6ba18f2
Yash Patni <ypatni3@gmail.com>
2020-08-04 11:23:25 -0400

    including tfidf and using pipeline to check for improved accuracy

--- Intro To Machine Learning/Natural Language Processing/yelp_reviews_classification.py
+++ Intro To Machine Learning/Natural Language Processing/yelp_reviews_classification.py
@@ -11,6 +11,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 mpl.rcParams['patch.force_edgecolor'] = True
 sns.set()
@@ -46,5 +47,25 @@ x = cv.fit_transform(x)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=101)
 nb = MultinomialNB()
 nb.fit(x_train, y_train)
+predictions = nb.predict(x_test)
+print(classification_report(y_test, predictions)) 
+print(confusion_matrix(y_test, predictions)) 
 
 
+
+
+#including tfidf and using pipeline
+pipeline = Pipeline([
+    ('bow', CountVectorizer()), 
+    ('tfidf', TfidfTransformer()), #integers to weighted tfidf scores 
+    ('model', MultinomialNB())
+])
+
+x = yelp_class['text']
+y = yelp_class['stars']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=101)
+pipeline.fit(x_train, y_train)
+predictions = pipeline.predict(x_test)
+print(classification_report(y_test, predictions)) 
+print(confusion_matrix(y_test, predictions)) 
+#tfidf made it worse!
