fcbddb586134a8a125e2aab65761dc84b108a4b9
Yash Patni <ypatni3@gmail.com>
2020-08-08 18:59:51 -0400

    creating and training keras based model

--- Neural Networks and Deep Learning/TensorFlow and Keras/tf_basics.py
+++ Neural Networks and Deep Learning/TensorFlow and Keras/tf_basics.py
@@ -5,13 +5,15 @@ import matplotlib as mpl
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import tensorflow as tf 
 mpl.rcParams['patch.force_edgecolor'] = True
 sns.set()
 sns.set_style('whitegrid')
 #very fake dataset
 
-df = pd.read_csv('~/Desktop/Code/Udemy DS + ML/Neural Networks and Deep Learning/Tensorflow/DATA/fake_reg.csv')
+df = pd.read_csv('~/Desktop/Code/Udemy DS + ML/Neural Networks and Deep Learning/Tensorflow and Keras/DATA/fake_reg.csv')
 print(df.head())
 sns.pairplot(df)
 plt.show()
@@ -30,6 +32,20 @@ scaler.fit(X_train) #calculates params needed to perform the actual scaling
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 print(X_train.max())
+
+#creating and training a keras based model 
+
+# help(Sequential) - really good documentation 
+# model = Sequential([Dense(4, activation='relu'),Dense(2, activation='relu'), Dense(1)])
+
+#adding layers one at a time makes it easier to edit 
+model = Sequential()
+model.add(Dense(4, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(4, activation='relu'))
+model.add(Dense(1)) #we only want to predict one value, the price
+
+model.compile(optimizer='rmsprop', loss= 'mse') #loss depends on what type of task you try to solve
 
 
 
