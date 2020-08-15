e0d7b9dc0c0d41e7fee07c05893a87b790dba26a
Yash Patni <ypatni3@gmail.com>
2020-08-15 16:40:22 -0400

    evaulating  model's accurancy through the errors and plotting

--- Neural Networks and Deep Learning/TensorFlow and Keras/tf_housing_project.py
+++ Neural Networks and Deep Learning/TensorFlow and Keras/tf_housing_project.py
@@ -5,7 +5,7 @@ import matplotlib as mpl
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, explained_variance_score
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 import tensorflow as tf 
@@ -55,9 +55,9 @@ plt.figure(figsize=(10,6))
 sns.boxplot(x='month', y='price', data = df)
 plt.show()
 #or we could just do 
-df.groupby('month').mean()['price']).plot()
+df.groupby('month').mean()['price'].plot()
 plt.show()
-df.groupby('year').mean()['price']).plot()
+df.groupby('year').mean()['price'].plot()
 plt.show()
 df=df.drop('date', axis = 1)
 df = df.drop('zipcode', axis=1)
@@ -67,7 +67,7 @@ df = df.drop('zipcode', axis=1)
 X = df.drop('price', axis = 1).values
 y = df['price'].values
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3, random_state=101)
-print(X_train.shape())
+print(X_train.shape)
 scaler = MinMaxScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
@@ -78,7 +78,21 @@ model.add(Dense(19, activation='relu'))
 model.add(Dense(19, activation='relu'))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
-model.fit(x=X_train, y = y_train, validation_data =(X_test, y_test).values ,batch_size=128, epochs = 400)
+model.fit(x=X_train, y = y_train, validation_data =(X_test, y_test) ,batch_size=128, epochs = 400)
+
+#model evaluations and predictions
+loss_df = pd.DataFrame(model.history.history)
+print(loss_df) #now we get the loss on the training set and val_loss - the  lost on the test set in order to see if you're over fitting 
+loss_df.plot()
+plt.show()
+predictions = model.predict(X_test)
+print(np.sqrt(mean_squared_error(y_test, predictions)))
+print(mean_absolute_error(y_test,predictions))
+
+print(explained_variance_score(y_test, predictions))
+plt.scatter(y_test, predictions)
+plt.plot(y_test, y_test, 'r')
+plt.show()
 
 
 
