18dcea9ca216df4838802bce9bedf534a597d7df
Yash Patni <ypatni3@gmail.com>
2020-08-14 21:39:23 -0400

    data preprocessing and train test split

--- Neural Networks and Deep Learning/TensorFlow and Keras/tf_housing_project.py
+++ Neural Networks and Deep Learning/TensorFlow and Keras/tf_housing_project.py
@@ -55,12 +55,23 @@ plt.figure(figsize=(10,6))
 sns.boxplot(x='month', y='price', data = df)
 plt.show()
 #or we could just do 
-print(df.groupby('month').mean()['price']).plot()
+df.groupby('month').mean()['price']).plot()
 plt.show()
-print(df.groupby('year').mean()['price']).plot()
+df.groupby('year').mean()['price']).plot()
 plt.show()
 df=df.drop('date', axis = 1)
 df = df.drop('zipcode', axis=1)
+
+#data preprocessing
+
+X = df.drop('price', axis = 1).values
+y = df['price'].values
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.3, random_state=101)
+print(X_train.shape())
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
 
 
 
