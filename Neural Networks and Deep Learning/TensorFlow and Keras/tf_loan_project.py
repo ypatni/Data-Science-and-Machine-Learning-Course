5d8bff86586ef112ba89a65a28f11497c8158024
Yash Patni <ypatni3@gmail.com>
2020-08-18 21:49:30 -0400

    some more data analysis for clarity

--- Neural Networks and Deep Learning/TensorFlow and Keras/tf_loan_project.py
+++ Neural Networks and Deep Learning/TensorFlow and Keras/tf_loan_project.py
@@ -33,3 +33,18 @@ plt.show()
 feat_info('installment')
 sns.scatterplot(x='installment', y = 'loan_amnt', data=df, alpha =0.6, palette='RdYlGn')
 plt.show()
+print(df.groupby('loan_status')['loan_amnt'].describe())
+#grades and subgrades 
+print(df['sub_grade'].unique())
+sns.countplot(x='grade',data=df,hue='loan_status')
+plt.show()
+plt.figure(figsize=(12,4))
+subgrade_order = sorted(df['sub_grade'].unique())
+sns.countplot(x='sub_grade',data=df,order = subgrade_order,palette='coolwarm' )
+plt.show()
+f_and_g = df[(df['grade']=='G') | (df['grade']=='F')]
+
+plt.figure(figsize=(12,4))
+subgrade_order = sorted(f_and_g['sub_grade'].unique())
+sns.countplot(x='sub_grade',data=f_and_g,order = subgrade_order,hue='loan_status')
+plt.show()
